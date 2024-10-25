423ec756ef2184455b4d6be2cc4e8f11d0f6348b
tfd-ed <info@tfdevs.com>
2024-10-25 11:08:20 +0700

    Socket Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Express Socket
+
+## Install and RUN
+```sh
+npm install
+```
+
+RUN
+```sh
+npm run dev
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require("socket.io");
+
+
+const app = express()
+const server = createServer(app);
+const io = new Server(server);
+
+// protocol: http, express
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+})
+
+// protocal: websocket, socket.io
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log("Server listening on 3000")
+})
